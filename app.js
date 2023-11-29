56c61f4f3c0dfd54b50f45f4b3acdf876468224c
mcm-ryan <mcm.ryan02@gmail.com>
2023-11-28 20:40:27 -0600

    adding js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-const sections = document.querySelectorAll('.section')
-const sectBtns = document.querySelectorAll('.controls')
-const sectBtn = document.querySelectorAll('.control')
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions() {
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function(){
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+        })
+    }
+
+    //sections active class
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id) {
+            // remove selected
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+
+            e.target.classList.add('active');
+
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransitions();
