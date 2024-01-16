72a25fc73284c91c76705cd1631f76bc70141819
mcm-ryan <mcm.ryan02@gmail.com>
2024-01-15 19:09:32 -0600

    added page transition function

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const sections = document.querySelectorAll('.section')
 const sectBtns = document.querySelectorAll('.controls')
 const sectBtn = document.querySelectorAll('.control')
+
+function pageTransitions(){
+    // Btn click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtns[i].addEventListener('click', () => {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('.active-btn', '');
+            this.className += 'active-btn';
+        })
+    }
+}
+pageTransitions();
